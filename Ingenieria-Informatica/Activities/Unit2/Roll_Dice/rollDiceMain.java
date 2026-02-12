eb156e82c643bce72c20590514cff491cccdda2e
LENOVO <LENOVO@DESKTOP-H75D55K>
2026-02-12 09:14:16 -0600

    rollDice activity

---
+++ Ingenieria-Informatica/Activities/Unit2/Roll_Dice/rollDiceMain.java
@@ -0,0 +1,37 @@
+package Unit2.Roll_Dice;
+
+import java.util.Scanner;
+
+public class rollDiceMain {
+	
+	public static void main(String[] args) {
+		int valord;
+		Scanner input;
+		
+		input = new Scanner(System.in);
+		System.out.println("Ingresar suma deseada");
+		valord = input.nextInt();
+		
+		System.out.println("Tiras un total de " + rollDice(valord) + " veces");
+	}
+	
+	public static int rollDice(int sumad) {
+		int dice1, dice2;
+		int i = 0;
+		
+		if(sumad >= 2 && sumad <= 12) {
+			do {
+				dice1 = (int)(Math.random() * 100 % 6 + 1);
+				dice2 = (int)(Math.random() * 100 % 6 + 1);
+				
+				++i;
+				
+				System.out.println("Los resultados del tiro fueron: dado 1 = " + dice1
+						+ ", dado 2 = " + dice2);
+				System.out.println("El numero de veces fue: " + i);
+			} while(dice1 + dice2 != sumad);
+		}
+		
+		return i;
+	}
+}
