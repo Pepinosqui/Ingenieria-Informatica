88c9a7d8483277e7d2cf73b7e9ab8a6beb5c4323
LENOVO <LENOVO@DESKTOP-H75D55K>
2026-02-28 21:57:12 -0600

    26/02/2026 class

--- Ingenieria-Informatica/Codes/classesIntroduction/Circle.java
+++ Ingenieria-Informatica/Codes/classesIntroduction/Circle.java
@@ -1,17 +1,55 @@
 package classesIntroduction;
 
+import java.util.Calendar;
+
 public class Circle {
-	double radius;
+	// Class parameters
+	private static final double PI = 3.141592;
+	
+	private double radius;
+	Calendar dateCreated = Calendar.getInstance();
+	public static int nCircles;
+	
+	
+	// Constructors
 	
 	public Circle() {
 		radius = 1.0;
+		++nCircles;
 	}
 	
-	public Circle(double assignRadius) {
-		radius = assignRadius;
+	public Circle(double radius) {
+		this.radius = radius;
+		++nCircles;
+	}
+	
+	
+	// Getters
+	
+	public double getRadius() {
+		return radius;
 	}
 	
 	public double getArea() {
-		return Math.PI * Math.pow(radius, 2.0);
+		return PI * radius * radius;
+	}
+	
+	
+	// Setters
+	
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	
+	// Other methods
+	
+	public static void printNCircles() {
+		System.out.println(nCircles);
+	}
+	
+	public void printDateCreated() {
+		System.out.println(Calendar.SECOND + ":" +
+				dateCreated.get(Calendar.MINUTE) + ":" + dateCreated.get(Calendar.SECOND));
 	}
 }
--- Ingenieria-Informatica/Codes/classesIntroduction/ClassesIntroductionMain.java
+++ Ingenieria-Informatica/Codes/classesIntroduction/ClassesIntroductionMain.java
@@ -2,19 +2,24 @@ package classesIntroduction;
 
 public class ClassesIntroductionMain {
 	public static void main(String[] args) {
-		Rectangle rec1 = new Rectangle(4.0 , 40.0);
-		Rectangle rec2 = new Rectangle(3.5 , 35.9);
+		Circle circ1 = new Circle();
+		System.out.println(circ1.getArea());
+		circ1.printDateCreated();
+		Circle.printNCircles();
 		
-		System.out.println("The first rectangle has:");
-		System.out.println(" Width = " + rec1.width);
-		System.out.println(" Heigth = " + rec1.heigth);
-		System.out.println(" Area = " + rec1.getArea());
-		System.out.println(" Perimeter = " + rec1.getPerimeter());
+		System.out.println(circ1.dateCreated.getTimeInMillis());
 		
-		System.out.println("\nThe second rectangle has:");
-		System.out.println(" Width = " + rec2.width);
-		System.out.println(" Heigth = " + rec2.heigth);
-		System.out.println(" Area = " + rec2.getArea());
-		System.out.println(" Perimeter = " + rec2.getPerimeter());
+		try {
+			Thread.sleep(2000);
+		} catch(InterruptedException e) {
+			System.out.println("NEIN");
+		}
+		
+		Circle circ2 = new Circle(14.6);
+		System.out.println(circ2.getArea());
+		circ1.printDateCreated();
+		Circle.printNCircles();
+		
+		System.out.println(circ2.dateCreated.getTimeInMillis());
 	}
 }
